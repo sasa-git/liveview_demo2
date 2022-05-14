813f6435d1b205ad61dc9d7b92e9b5dd0b975580
sasa <sasasoni1119@gmail.com>
2022-05-14 16:57:29 +0900

    practice: add basic component

---
+++ lib/chirp_web/live/component_live/modal_component.ex
@@ -0,0 +1,15 @@
+defmodule ModalComponent do
+  use Phoenix.LiveComponent
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div id={@id}>
+      <%= render_block(@inner_block, []) %>
+    </div>
+    """
+  end
+end
